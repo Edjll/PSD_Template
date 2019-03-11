fe67a734954c7dc93808ba30e0c8365d06d0e007
Edjll <48223023+Edjll@users.noreply.github.com>
2019-03-11 23:36:54 +0400

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,188 @@ menu.onclick = function nav() {
 		cl.className = "nav";
 	}
 }
+
+search.onclick = function search() {
+	var cl = document.getElementById('searc');
+
+	if (cl.className === "sear") {
+		cl.className = "sear-act";
+	} else {
+		cl.className = "sear";
+	}
+}
+
+ph.onclick = function sop() {
+	var i = 1;
+	do {
+		var z = "Photography-" + i;
+		var elem = document.getElementById(z);
+		if (elem === null) break;
+		elem.className = "d";
+		i++;
+	} while (true);
+	i = 1;
+	do {
+		var z = "WebD-" + i;
+		var elem = document.getElementById(z);
+		if (elem === null) break;
+		elem.className = "dNew";
+		i++;
+	} while (true);
+	i = 1;
+	do {
+		var z = "Illust-" + i;
+		var elem = document.getElementById(z);
+		if (elem === null) break;
+		elem.className = "dNew";
+		i++;
+	} while (true);
+	i = 1;
+	do {
+		var z = "MobA-" + i;
+		var elem = document.getElementById(z);
+		if (elem === null) break;
+		elem.className = "dNew";
+		i++;
+	} while (true);
+}
+
+wd.onclick = function sop() {
+	var i = 1;
+	do {
+		var z = "WebD-" + i;
+		var elem = document.getElementById(z);
+		if (elem === null) break;
+		elem.className = "d";
+		i++;
+	} while (true);
+	i = 1;
+	do {
+		var z = "Photography-" + i;
+		var elem = document.getElementById(z);
+		if (elem === null) break;
+		elem.className = "dNew";
+		i++;
+	} while (true);
+	i = 1;
+	do {
+		var z = "Illust-" + i;
+		var elem = document.getElementById(z);
+		if (elem === null) break;
+		elem.className = "dNew";
+		i++;
+	} while (true);
+	i = 1;
+	do {
+		var z = "MobA-" + i;
+		var elem = document.getElementById(z);
+		if (elem === null) break;
+		elem.className = "dNew";
+		i++;
+	} while (true);
+}
+
+ma.onclick = function sop() {
+	var i = 1;
+	do {
+		var z = "MobA-" + i;
+		var elem = document.getElementById(z);
+		if (elem === null) break;
+		elem.className = "d";
+		i++;
+	} while (true);
+	i = 1;
+	do {
+		var z = "WebD-" + i;
+		var elem = document.getElementById(z);
+		if (elem === null) break;
+		elem.className = "dNew";
+		i++;
+	} while (true);
+	i = 1;
+	do {
+		var z = "Illust-" + i;
+		var elem = document.getElementById(z);
+		if (elem === null) break;
+		elem.className = "dNew";
+		i++;
+	} while (true);
+	i = 1;
+	do {
+		var z = "Photography-" + i;
+		var elem = document.getElementById(z);
+		if (elem === null) break;
+		elem.className = "dNew";
+		i++;
+	} while (true);
+}
+
+ill.onclick = function sop() {
+	var i = 1;
+	do {
+		var z = "Illust-" + i;
+		var elem = document.getElementById(z);
+		if (elem === null) break;
+		elem.className = "d";
+		i++;
+	} while (true);
+	i = 1;
+	do {
+		var z = "WebD-" + i;
+		var elem = document.getElementById(z);
+		if (elem === null) break;
+		elem.className = "dNew";
+		i++;
+	} while (true);
+	i = 1;
+	do {
+		var z = "MobA-" + i;
+		var elem = document.getElementById(z);
+		if (elem === null) break;
+		elem.className = "dNew";
+		i++;
+	} while (true);
+	i = 1;
+	do {
+		var z = "Photography-" + i;
+		var elem = document.getElementById(z);
+		if (elem === null) break;
+		elem.className = "dNew";
+		i++;
+	} while (true);
+}
+
+all.onclick = function sop() {
+	var i = 1;
+	do {
+		var z = "WebD-" + i;
+		var elem = document.getElementById(z);
+		if (elem === null) break;
+		elem.className = "d";
+		i++;
+	} while (true);
+	i = 1;
+	do {
+		var z = "MobA-" + i;
+		var elem = document.getElementById(z);
+		if (elem === null) break;
+		elem.className = "d";
+		i++;
+	} while (true);
+	i = 1;
+	do {
+		var z = "Photography-" + i;
+		var elem = document.getElementById(z);
+		if (elem === null) break;
+		elem.className = "d";
+		i++;
+	} while (true);
+	i = 1;
+	do {
+		var z = "Illust-" + i;
+		var elem = document.getElementById(z);
+		if (elem === null) break;
+		elem.className = "d";
+		i++;
+	} while (true);
+}
